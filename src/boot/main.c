1e1ad2c987905176f573648afd36eccbea012602
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-07-25 09:24:58 -0600

    Wait for user input to exit main.c

--- src/boot/main.c
+++ src/boot/main.c
@@ -52,13 +52,31 @@ EFI_STATUS
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 	InitializeLib(ImageHandle, SystemTable);
+	EFI_STATUS Status;
 
+	Print(L"\n--== Welcome to the kernel loader ==--\n");
 	Print(L"Loading kernel...\n");
-	if (LoadFile(NULL, L"kernel.elf", ImageHandle) == NULL)
+	EFI_FILE *Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle);
+	if (Kernel == NULL)
 		Print(L"Could not load the kernel\n");
 	else
-		Print(L"Kernel loaded successfully\n");
+		Print(L"Kernel loaded successfully\n\n");
 
-	while(1);
+	Print(L"Press any key to exit...\n");
+	EFI_INPUT_KEY Key;
+	/* Now wait for a keystroke before continuing, otherwise your
+	   message will flash off the screen before you see it.
+
+	   First, we need to empty the console input buffer to flush
+	   out any keystrokes entered before this point */
+	Status = ST->ConIn->Reset(ST->ConIn, FALSE);
+	if (EFI_ERROR(Status))
+		return Status;
+
+	/* Now wait until a key becomes available.  This is a simple
+	   polling implementation.  You could try and use the WaitForKey
+	   event instead if you like */
+	while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY);
+
 	return EFI_SUCCESS;
 }
