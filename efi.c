35d4a9d98fa107956616a17b74de5432b09fa582
caden <cadenr.maxwell@gmail.com>
2025-04-06 15:58:32 -0400

    Boot services definition

--- efi.c
+++ efi.c
@@ -11,14 +11,22 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     CHAR16 *string = u"Hello, world!\r\n";
     SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
 
-    string = u"Press any key to exit...\r\n";
+    string = u"Press a key and see what happens: ";
     SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
 
-    // Keep reading until key has been pressed
+    // Wait until key has been pressed
+    SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, NULL);
+
+    // Read in the key and print it
     EFI_INPUT_KEY Key;
     Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
-    while (EFI_ERROR(Status))
-        Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, &Key.UnicodeChar);
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, u"\r\n");
+
+    // Wait until user presses key to exit
+    string = u"Press any key to exit...\r\n";
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
+    SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, NULL);
 
     return EFI_SUCCESS;
 }
