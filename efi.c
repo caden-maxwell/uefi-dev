3a0e2c57f72c0a5166bb589411aadde71943f8c2
caden <cadenr.maxwell@gmail.com>
2025-04-06 15:33:12 -0400

    Filling out headers; simple hello world

--- efi.c
+++ efi.c
@@ -1,10 +1,24 @@
 #include "efi.h"
 
+EFI_STATUS Status;
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    (void)ImageHandle, (void)SystemTable;
-    EFI_STATUS Status = EFI_COMPROMISED_DATA;
-    if (EFI_ERROR(Status))
-        return Status;
+    (void)ImageHandle;
+
+    // Clear screen, set cursor position to (0,0), etc...
+    Status = SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
+
+    CHAR16 *string = u"Hello, world!\r\n";
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
+
+    string = u"Press any key to exit...\r\n";
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
+
+    // Keep reading until key has been pressed
+    EFI_INPUT_KEY Key;
+    Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
+    while (EFI_ERROR(Status))
+        Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
 
     return EFI_SUCCESS;
 }
--- efi.h
+++ efi.h
@@ -6,6 +6,8 @@
 #define EFIAPI __attribute__((__ms_abi__))
 #define PACKED __attribute__((__packed__))
 #define ALIGNED4 __attribute__((__aligned__(4)))
+#define FALSE (BOOLEAN)(0==1)
+#define TRUE  (BOOLEAN)(0==0)
 
 // UEFI Spec 2.3.1
 #define IN
@@ -28,16 +30,7 @@ typedef uint8_t  CHAR8;
 typedef uint16_t char16_t; // Might need uint_least16_t here
 typedef char16_t CHAR16;
 
-typedef void VOID;
-typedef struct {
-    UINT32 TimeLow;
-    UINT16 TimeMid;
-    UINT16 TimeHighAndVersion;
-    UINT8  ClockSeqHighAndReserved;
-    UINT8  ClockSeqLow;
-    UINT8  Node[6];
-} PACKED EFI_GUID; // UEFI Spec. Appendix A
-
+typedef void   VOID;
 typedef UINTN  EFI_STATUS;
 typedef VOID  *EFI_HANDLE;
 typedef VOID  *EFI_EVENT;
@@ -47,6 +40,16 @@ typedef UINT8  EFI_MAC_ADDRESS[32];
 typedef UINT8  EFI_IPv4_ADDRESS[4];
 typedef UINT8  EFI_IPv6_ADDRESS[16];
 typedef UINT8  ALIGNED4 EFI_IP_ADDRESS[16];
+
+// UEFI Spec. Appendix A
+typedef struct {
+    UINT32 TimeLow;
+    UINT16 TimeMid;
+    UINT16 TimeHighAndVersion;
+    UINT8  ClockSeqHighAndReserved;
+    UINT8  ClockSeqLow;
+    UINT8  Node[6];
+} PACKED EFI_GUID;
 
 // UEFI Spec. Appendix D
 #define HIGH_BIT 0x8000000000000000ULL
@@ -88,14 +91,145 @@ typedef UINT8  ALIGNED4 EFI_IP_ADDRESS[16];
 #define EFI_IP_ADDRESS_CONFLICT  ENCODE_ERROR (34)
 #define EFI_HTTP_ERROR           ENCODE_ERROR (35)
 
-typedef void EFI_SYSTEM_TABLE;
+typedef struct _EFI_SYSTEM_TABLE EFI_SYSTEM_TABLE;
 
 // UEFI Spec. 4.1.1
 typedef
 EFI_STATUS
 (EFIAPI *EFI_IMAGE_ENTRY_POINT) (
-    IN EFI_HANDLE                ImageHandle,
-    IN EFI_SYSTEM_TABLE          *SystemTable
-    );
+    IN EFI_HANDLE       ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable
+);
+
+// UEFI Spec 4.2.1
+typedef struct {
+    UINT64 Signature;
+    UINT32 Revision;
+    UINT32 HeaderSize;
+    UINT32 CRC32;
+    UINT32 Reserved;
+} EFI_TABLE_HEADER;
+
+// -------------------------------------------------------------
+// ------ EFI_SIMPLE_TEXT_INPUT_PROTOCOL - UEFI Spec 12.3 ------
+// -------------------------------------------------------------
+
+typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// UEFI Spec 12.3.2
+typedef
+EFI_STATUS
+(EFIAPI *EFI_INPUT_RESET) (
+    IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This,
+    IN BOOLEAN                        ExtendedVerification
+);
+
+// UEFI Spec 12.3.3
+typedef struct {
+    UINT16 ScanCode;
+    CHAR16 UnicodeChar;
+} EFI_INPUT_KEY;
+
+typedef
+EFI_STATUS
+(EFIAPI *EFI_INPUT_READ_KEY) (
+    IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This,
+    OUT EFI_INPUT_KEY                 *Key
+);
+
+// UEFI Spec 12.3.1
+typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
+    EFI_INPUT_RESET    Reset;
+    EFI_INPUT_READ_KEY ReadKeyStroke;
+    EFI_EVENT          WaitForKey;
+} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// --------------------------------------------------------------
+// ------ EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL - UEFI Spec 12.4 ------
+// --------------------------------------------------------------
+
+typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+// UEFI Spec 12.4.2
+typedef
+EFI_STATUS
+(EFIAPI *EFI_TEXT_RESET)(
+    IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+    IN BOOLEAN                         ExtendedVerification
+);
+
+// UEFI Spec 12.4.3
+typedef
+EFI_STATUS
+(EFIAPI *EFI_TEXT_STRING)(
+    IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+    IN CHAR16                          *String
+);
+
+// UEFI Spec 12.4.8
+typedef
+EFI_STATUS
+(EFIAPI *EFI_TEXT_CLEAR_SCREEN) (
+    IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
+);
+
+// UEFI Spec 12.4.1
+typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+    EFI_TEXT_RESET               Reset;
+    EFI_TEXT_STRING              OutputString;
+    void                         *TestString;// EFI_TEXT_TEST_STRING         TestString;
+    void                         *QueryMode;// EFI_TEXT_QUERY_MODE          QueryMode;
+    void                         *SetMode;// EFI_TEXT_SET_MODE            SetMode;
+    void                         *SetAttribute;// EFI_TEXT_SET_ATTRIBUTE       SetAttribute;
+    EFI_TEXT_CLEAR_SCREEN        ClearScreen;
+    void                         *SetCursorPosition;// EFI_TEXT_SET_CURSOR_POSITION SetCursorPosition;
+    void                         *EnableCursor;// EFI_TEXT_ENABLE_CURSOR       EnableCursor;
+    void                         *Mode;// SIMPLE_TEXT_OUTPUT_MODE      *Mode;
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+// ----------------------------------
+// ------ EFI_RUNTIME_SERVICES ------
+// ----------------------------------
+
+typedef struct _EFI_RUNTIME_SERVICES {
+
+} EFI_RUNTIME_SERVICES;
+
+// -------------------------------
+// ------ EFI_BOOT_SERVICES ------
+// -------------------------------
+
+typedef struct _EFI_BOOT_SERVICES {
+
+} EFI_BOOT_SERVICES;
+
+// -------------------------------------
+// ------ EFI_CONFIGURATION_TABLE ------
+// -------------------------------------
+
+typedef struct _EFI_CONFIGURATION_TABLE {
+
+} EFI_CONFIGURATION_TABLE;
+
+// ------------------------------
+// ------ EFI_SYSTEM_TABLE ------
+// ------------------------------
+
+// UEFI Spec 4.3.1
+typedef struct _EFI_SYSTEM_TABLE {
+  EFI_TABLE_HEADER                Hdr;
+  CHAR16                          *FirmwareVendor;
+  UINT32                          FirmwareRevision;
+  EFI_HANDLE                      ConsoleInHandle;
+  EFI_SIMPLE_TEXT_INPUT_PROTOCOL  *ConIn;
+  EFI_HANDLE                      ConsoleOutHandle;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+  EFI_HANDLE                      StandardErrorHandle;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *StdErr;
+  EFI_RUNTIME_SERVICES            *RuntimeServices;
+  EFI_BOOT_SERVICES               *BootServices;
+  UINTN                           NumberOfTableEntries;
+  EFI_CONFIGURATION_TABLE         *ConfigurationTable;
+} EFI_SYSTEM_TABLE;
 
 #endif // _EFI_H
