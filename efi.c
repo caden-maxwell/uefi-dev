706d57ae9de41c715faf08d15fc67251a8539d9c
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2025-04-06 17:56:46 -0400

    More Simple Text Output Protocol; make ldflags

--- efi.c
+++ efi.c
@@ -4,29 +4,33 @@ EFI_STATUS Status;
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     (void)ImageHandle;
+    EFI_SYSTEM_TABLE  *ST = SystemTable;
+    EFI_BOOT_SERVICES *BS = ST->BootServices;
 
     // Clear screen, set cursor position to (0,0), etc...
-    Status = SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
+    Status = ST->ConOut->Reset(ST->ConOut, FALSE);
 
-    CHAR16 *string = u"Hello, world!\r\n";
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
+    INT32 TextMode = SystemTable->ConOut->Mode->Mode;
+    CHAR16 mode = (CHAR16)TextMode + 48;
+    ST->ConOut->OutputString(ST->ConOut, u"DEVICE INFO:\r\n\tTEXT_MODE: ");
+    ST->ConOut->OutputString(ST->ConOut, &mode);
+    ST->ConOut->OutputString(ST->ConOut, u"\r\n");
 
-    string = u"Press a key and see what happens: ";
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
+    ST->ConOut->OutputString(ST->ConOut, u"Press a key...\r\n");
 
     // Wait until key has been pressed
-    SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, NULL);
+    BS->WaitForEvent(1, &ST->ConIn->WaitForKey, NULL);
 
     // Read in the key and print it
     EFI_INPUT_KEY Key;
-    Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, &Key.UnicodeChar);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, u"\r\n");
+    ST->ConIn->ReadKeyStroke(ST->ConIn, &Key);
+    ST->ConOut->OutputString(ST->ConOut, u"You pressed: '");
+    ST->ConOut->OutputString(ST->ConOut, &Key.UnicodeChar);
+    ST->ConOut->OutputString(ST->ConOut, u"'\r\n");
 
     // Wait until user presses key to exit
-    string = u"Press any key to exit...\r\n";
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, string);
-    SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, NULL);
+    ST->ConOut->OutputString(ST->ConOut, u"Press any key to exit...\r\n");
+    BS->WaitForEvent(1, &ST->ConIn->WaitForKey, NULL);
 
     return EFI_SUCCESS;
 }
