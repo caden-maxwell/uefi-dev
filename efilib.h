01d6a4306fd59acc5e264843d36e42adf81a009d
caden <cadenr.maxwell@gmail.com>
2025-04-06 21:16:09 -0400

    clean up

--- efilib.h
+++ efilib.h
@@ -11,6 +11,7 @@ extern EFI_SIMPLE_TEXT_INPUT_PROTOCOL *cIn;
 extern EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *cOut;
 extern EFI_STATUS Status;
 
-void init_global_vars(EFI_SYSTEM_TABLE *SystemTable);
+void InitGlobalVars(EFI_SYSTEM_TABLE *SystemTable);
+BOOLEAN Printf(CHAR16 *fmt, ...);
 
 #endif //_LIB_H
