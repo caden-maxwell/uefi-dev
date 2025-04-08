6984f27b24c5f8f896074936e2dfb65ed2d9564f
caden <cadenr.maxwell@gmail.com>
2025-04-08 19:54:06 -0400

    Includes, formatting

--- efi.c
+++ efi.c
@@ -1,4 +1,3 @@
-#include "efi.h"
 #include "efilib.h"
 
 EFI_STATUS UefiEntry(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
@@ -9,7 +8,7 @@ EFI_STATUS UefiEntry(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     // Clear screen, set cursor position to (0,0), etc...
     Status = cOut->Reset(cOut, FALSE);
 
-    // Set colors
+    // Set colors: blue letters, gray background
     cOut->SetAttribute(cOut, EFI_TEXT_ATTR(EFI_BLUE, EFI_LIGHTGRAY));
     cOut->ClearScreen(cOut);
 
@@ -22,10 +21,10 @@ EFI_STATUS UefiEntry(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     Printf(u"- s: Hello, %s\r\n", u"world!");
     Printf(u"- c: '%c' is the first letter of the alphabet.\r\n", 'A');
     Printf(u"- d: There are %d feet in a mile.\r\n", 5280);
-    Printf(u"- d: MAX_INT: %d\r\n", 2147483647);
+    Printf(u"- d: -MAX_INT: %d\r\n", -2147483647);
     Printf(u"- u x: %u is 0x%x in hexadecimal.\r\n", 0xDEADBEEF, 0xDEADBEEF);
+    Printf(u"- s c d u x: %s %c %u %d 0x%x\r\n", u"Hello", 'Z', 0xFFFFFFFF, 0x1000, 4096);
     Printf(u"- r: This %r ain't right...\r\n", 0);
-    Printf(u"- s c d u x h: %s %c %u %d 0x%x %h\r\n", u"Hello", 'Z', 0xFFFFFFFF, 0x1000, 3987, 23);
     Printf(u"------------------------------\r\n\n");
 
     // Wait until key has been pressed
