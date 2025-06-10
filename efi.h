32087c75cc3aba097c5406a2f513ab6e461cd09c
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2025-06-09 22:51:56 -0400

    Add graphics output protocol stuffs

--- efi.h
+++ efi.h
@@ -279,6 +279,110 @@ typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     SIMPLE_TEXT_OUTPUT_MODE      *Mode;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
+// ------------------------------------------
+// ------ EFI_GRAPHICS_OUTPUT_PROTOCOL ------
+// ------------------------------------------
+
+typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL EFI_GRAPHICS_OUTPUT_PROTOCOL;
+
+// Uefi Spec 12.9.2
+typedef struct {
+    UINT32 RedMask;
+    UINT32 GreenMask;
+    UINT32 BlueMask;
+    UINT32 ReservedMask;
+} EFI_PIXEL_BITMASK;
+
+// Uefi Spec 12.9.2
+typedef enum {
+    PixelRedGreenBlueReserved8BitPerColor,
+    PixelBlueGreenRedReserved8BitPerColor,
+    PixelBitMask,
+    PixelBltOnly,
+    PixelFormatMax
+} EFI_GRAPHICS_PIXEL_FORMAT;
+
+// Uefi Spec 12.9.2
+typedef struct {
+    UINT32                    Version;
+    UINT32                    HorizontalResolution;
+    UINT32                    VerticalResolution;
+    EFI_GRAPHICS_PIXEL_FORMAT PixelFormat;
+    EFI_PIXEL_BITMASK         PixelInformation;
+    UINT32                    PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
+
+// Uefi Spec 12.9.2.1
+typedef
+EFI_STATUS
+(EFIAPI *EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE) (
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL          *This,
+    IN UINT32                                ModeNumber,
+    OUT UINTN                                *SizeOfInfo,
+    OUT EFI_GRAPHICS_OUTPUT_MODE_INFORMATION **Info
+);
+
+// Uefi Spec 12.9.2.2
+typedef
+EFI_STATUS
+(EFIAPI *EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE) (
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+    IN UINT32                       ModeNumber
+);
+
+// Uefi Spec 12.9.2.3
+typedef struct {
+    UINT8 Blue;
+    UINT8 Green;
+    UINT8 Red;
+    UINT8 Reserved;
+} EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
+
+// Uefi Spec 12.9.2.3
+typedef enum {
+    EfiBltVideoFill,
+    EfiBltVideoToBltBuffer,
+    EfiBltBufferToVideo,
+    EfiBltVideoToVideo,
+    EfiGraphicsOutputBltOperationMax
+} EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
+
+// Uefi Spec 12.9.2.3
+typedef
+EFI_STATUS
+(EFIAPI *EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT) (
+    IN EFI_GRAPHICS_OUTPUT_PROTOCOL      *This,
+    IN OUT EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer, OPTIONAL
+    IN EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation,
+    IN UINTN                             SourceX,
+    IN UINTN                             SourceY,
+    IN UINTN                             DestinationX,
+    IN UINTN                             DestinationY,
+    IN UINTN                             Width,
+    IN UINTN                             Height,
+    IN UINTN                             Delta OPTIONAL
+);
+
+typedef UINT64 EFI_PHYSICAL_ADDRESS;
+
+// Uefi Spec 12.9.2
+typedef struct {
+    UINT32                               MaxMode;
+    UINT32                               Mode;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
+    UINTN                                SizeOfInfo;
+    EFI_PHYSICAL_ADDRESS                 FrameBufferBase;
+    UINTN                                FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
+
+// Uefi Spec 12.9.2
+typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL {
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE QueryMode;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE   SetMode;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT        Blt;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE       *Mode;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL;
+
 // --------------------------------------
 // ------ EFI_DEVICE_PATH_PROTOCOL ------
 // --------------------------------------
@@ -508,8 +612,6 @@ typedef enum {
     EfiUnacceptedMemoryType,
     EfiMaxMemoryType
 } EFI_MEMORY_TYPE;
-
-typedef UINT64 EFI_PHYSICAL_ADDRESS;
 
 // UEFI Spec 7.2.1
 typedef
